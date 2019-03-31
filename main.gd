fa2b2fc38026073176d12ee31631c5c74e703d30
quendera <tiagoquendera@gmail.com>
2019-03-31 05:06:58 +0100

    v0.1

--- main.gd
+++ main.gd
@@ -3,32 +3,43 @@ extends Node
 var effect
 var recording
 var letter_index
-# var letters = ["phi", "beta", "gamma", "mu", "lambda", "psi", "theta", "delta"]
+var tracknr
+var letters = {"phi" : "φ" , "beta" : "β", "gamma" : "γ" , "mu" : "μ", "lambda" : "λ" , "psi" : "ψ", "theta" : "θ", "delta" : "δ"}
 
 func _ready():
-	$PathA.stream = load("res://assets/audio/PathA.wav")
-	$PathB.stream = load("res://assets/audio/PathB.wav")
-	$PathC.stream = load("res://assets/audio/PathC.wav")
-	$PathD.stream = load("res://assets/audio/PathD.wav")
-	
+	init()
+
+
+func init():
 	var rng = randf()
+	print(rng)
+	
+	$Timer2.set_wait_time(10)
+	$Timer2.set_one_shot(1)
+	$Timer2.start()
 	
 	if rng < 0.25:
-		$PathA.play(0.0)
-	elif 0.25 < rng < 0.50:
-		$PathB.play(0.0)
-	elif 0.50 < rng < 0.75:
-		$PathC.play(0.0)
+		$PathA.play()
+		tracknr = 1
+	elif rng > 0.25 && rng < 0.50:
+		$PathB.play()
+		tracknr = 2
+	elif rng > 0.50 && rng < 0.75:
+		$PathC.play()
+		tracknr = 3
 	else:
-		$PathD.play(0.0)
-	
+		$PathD.play()
+		tracknr = 4
+
+func _on_Timer2_timeout():
 	letter_index = "phi" #sets first letter to phi
 	play_letter(letter_index)
-
 
 func play_letter(letter_index):
 	print(letter_index)
 	start_recording()
+	$AnimationPlayer.play("letras")
+	$Text.set_text(letters.get(letter_index))
 	$PlayLetters.stream = load("res://assets/audio/" + letter_index + ".wav")
 	$PlayLetters.play(0.0)
 
@@ -36,7 +47,7 @@ func play_letter(letter_index):
 func start_recording():
 	var index = AudioServer.get_bus_index("Record")
 	effect = AudioServer.get_bus_effect(index, 0)
-	$Timer.set_wait_time(5)
+	$Timer.set_wait_time(56)
 	$Timer.set_one_shot(1)
 	$Timer.start()
 	effect.set_recording_active(true)
@@ -52,31 +63,45 @@ func _on_Timer_timeout():
 	
 	var rng = randf()
 	var idx = letter_index
-	print(idx)
+	
 	#LAYER 1
-	if idx == "phi":
-		if rng < 0.6:
+	if tracknr == 1:
+		if idx == "phi":
 			letter_index = "beta"
-		else:
-			letter_index = "gamma"
-		
-	#LAYER 2
-	elif idx == "beta":
-		if rng < 0.6:
-			letter_index = "mu"
-		else:
-			letter_index = "lambda"
 			
-	elif idx == "gamma":
-		if rng < 0.5:
-			letter_index = "psi"
-		else:
+		#LAYER 2
+		elif idx == "beta":
+			letter_index = "mu"
+			
+		#LAYER 3
+		elif idx == "mu":
+			letter_index = "gamma"
+			
+		elif idx == "gamma":
 			letter_index = "theta"
 			
-	#LAYER 3
-	elif idx == "mu":
-		letter_index = "delta"
+		elif idx == "theta":
+			letter_index = "psi"
+			
+		elif idx == "psi":
+			letter_index = "lambda"
+			
+		elif idx == "lambda":
+			letter_index = "delta"
 	else:
-		letter_index = "phi"
+		letter_index = letters.keys()[randi() % letters.keys().size()-1]
+		
 	
 	play_letter(letter_index)
+
+func _on_PathA_finished():
+	init()
+
+func _on_PathB_finished():
+	init()
+
+func _on_PathC_finished():
+	init()
+
+func _on_PathD_finished():
+	init()
